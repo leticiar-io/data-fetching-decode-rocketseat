b0f389a69032a9f9d40ae99f27a68c8c01c71294
leticiar-io <leticia.rodrigues20019@gmail.com>
2022-12-02 11:09:29 -0300

    adding react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import { useFetch } from "./hooks/useFetch";
+import { Route, Routes } from "react-router-dom";
+import { Repo } from "./pages/Repo";
+import Repos from "./pages/Repos";
 
-type Repository = {
-  full_name: string;
-  description: string;
-};
 
 function App() {
-  const { data: repositories, isFetching} = useFetch<Repository[]>("users/leticiar-io/repos");
-  
+
   return (
-    <ul>
-      { isFetching && <p>Carregando ...</p>}
-      {repositories?.map((repo) => {
-        return (
-          <li key={repo.full_name}>
-            <strong>{repo.full_name}</strong>
-            <p>{repo.description}</p>
-          </li>
-        );
-      })}
-    </ul>
+    <Routes>
+      <Route path = "/" element={<Repos/>} />
+      <Route path="/repos/*" element={<Repo/>} />
+    </Routes>
   );
 }
 
