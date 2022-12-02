9157be7f2df14b89fc6bbaab9fd45eb2ccb11045
leticiar-io <leticia.rodrigues20019@gmail.com>
2022-12-02 12:09:22 -0300

    adding the ChakraProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,18 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { queryClient } from "./services/queryClient";
 
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from "./styles/theme";
+
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App/>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
